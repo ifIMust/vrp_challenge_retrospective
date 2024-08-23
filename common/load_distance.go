ece20ed525bdf87930805296f155b82eed654009
Trent <42818748+ifIMust@users.noreply.github.com>
2024-08-23 17:19:25 -0600

    Try an approach that apparently isn't better than greedy.

--- common/load_distance.go
+++ common/load_distance.go
@@ -1,13 +1,17 @@
 package common
 
+import "math"
+
 // LoadDistance precomputes the distance from load dropoff to pickup
 type LoadDistance struct {
+	loads           LoadMap
 	distances       map[int]map[int]float64
 	averageDistance float64
 }
 
 func NewLoadDistance(loads []*Load) *LoadDistance {
 	ld := LoadDistance{}
+	ld.loads = AsMap(loads)
 	ld.distances = make(map[int]map[int]float64)
 	if len(loads) == 0 {
 		return &ld
@@ -30,10 +34,122 @@ func NewLoadDistance(loads []*Load) *LoadDistance {
 	return &ld
 }
 
+// Return precomputed distance from i's Dropoff to j's Pickup
 func (ld *LoadDistance) Distance(i, j int) float64 {
 	return ld.distances[i][j]
 }
 
 func (ld *LoadDistance) AverageDistance() float64 {
 	return ld.averageDistance
+}
+
+// Find the Load nearest to each the given index (Dropoff nearest to Pickup).
+// Exclude all indices found in exclude.
+// Return the index and the distance.
+// Prefers valid input. Returns 0 index if input is invalid
+func (ld *LoadDistance) Nearest(index int, exclude map[int]struct{}) (int, float64) {
+	distance := math.Inf(1)
+	nearest := 0
+	m, ok := ld.distances[index]
+	if !ok {
+		return nearest, distance
+	}
+
+	for i, d := range m {
+		_, excluded := exclude[i]
+		if !excluded && d < distance {
+			distance = d
+			nearest = i
+		}
+	}
+
+	return nearest, distance
+}
+
+// Like Nearest, but look for Load with closest Dropoff to the indexed Load's Pickup
+func (ld *LoadDistance) NearestBefore(index int, exclude map[int]struct{}) (int, float64) {
+	distance := math.Inf(1)
+	nearest := 0
+	for i, nestedMap := range ld.distances {
+		_, excluded := exclude[i]
+		if !excluded {
+			d := nestedMap[index]
+			if d < distance {
+				distance = d
+				nearest = i
+			}
+		}
+	}
+
+	return nearest, distance
+}
+
+// Find the pair of Loads nearest each other, excluding indices found in exclude.
+// Return the indices (in order) and their distance.
+// May return 0 indices if no pair is found (due to exclusion)
+func (ld *LoadDistance) NearestPair(exclude map[int]struct{}) (int, int, float64) {
+	minDistance := math.Inf(1)
+	nearestDrop := 0
+	nearestPick := 0
+
+	for i, nestedMap := range ld.distances {
+		_, iExcluded := exclude[i]
+		if !iExcluded {
+			for j, distance := range nestedMap {
+				_, jExcluded := exclude[j]
+				if !jExcluded && distance < minDistance {
+					minDistance = distance
+					nearestDrop = i
+					nearestPick = j
+				}
+			}
+		}
+	}
+	return nearestDrop, nearestPick, minDistance
+}
+
+// Use precomputed distances to determine the route cost
+func (ld *LoadDistance) RouteCost(routes [][]int) float64 {
+	drivers := 0
+	minutes := 0.0
+	for _, driver := range routes {
+		drivers += 1
+		minutes += ld.MinutesFromRoute(driver, true)
+	}
+	return float64(drivers)*500.0 + minutes
+}
+
+func (ld *LoadDistance) MinutesFromRoute(route []int, includeDepotTime bool) float64 {
+	minutes := 0.0
+	numLoads := len(route)
+	if numLoads > 0 {
+		if includeDepotTime {
+			minutes += ld.loads[route[0]].HomeCostPickup()
+			minutes += ld.loads[route[numLoads-1]].HomeCostDropoff()
+		}
+		lastLoad := route[0]
+		for _, l := range route {
+			minutes += ld.loads[l].Cost
+			if l != lastLoad {
+				minutes += ld.Distance(lastLoad, l)
+			}
+			lastLoad = l
+		}
+	}
+	return minutes
+}
+
+func (ld *LoadDistance) NearestPickupFromHome(exclude map[int]struct{}) int {
+	index := 0
+	closest := math.Inf(1)
+	for i, l := range ld.loads {
+		_, excluded := exclude[i]
+		if !excluded {
+			if l.HomeCostPickup() < closest {
+				closest = l.HomeCostPickup()
+				index = i
+			}
+		}
+	}
+	return index
 }
--- common/load_distance_test.go
+++ common/load_distance_test.go
@@ -1,6 +1,15 @@
 package common
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+const floatCompareThreshold = 1e-7
+
+func almostEqual(x float64, y float64) bool {
+	return math.Abs(x-y) <= floatCompareThreshold
+}
 
 func TestLoadDistanceNew(t *testing.T) {
 	loads := make([]*Load, 2)
@@ -38,5 +47,69 @@ func TestAverageDistance(t *testing.T) {
 
 	if 5 != avg {
 		t.Fatalf("average unexpected: %v", avg)
+	}
+}
+
+func TestNearest(t *testing.T) {
+	loads := make([]*Load, 3)
+	l0x := NewLocation(0, 0)
+	l0y := NewLocation(3, 4)
+	loads[0] = NewLoad(1, l0x, l0y)
+
+	l1x := NewLocation(3, 5)
+	l1y := NewLocation(6, 9)
+	loads[1] = NewLoad(2, l1x, l1y)
+
+	l2x := NewLocation(5, 5)
+	l2y := NewLocation(1, 2)
+	loads[2] = NewLoad(3, l2x, l2y)
+
+	exclude := make(map[int]struct{})
+	ld := NewLoadDistance(loads)
+	nearestIndex, distance := ld.Nearest(1, exclude)
+	if 2 != nearestIndex {
+		t.Fatalf("nearest index lookup incorrect")
+	}
+	if distance != 1 {
+		t.Fatalf("nearest distance incorrect")
+	}
+
+	nearestIndex, distance = ld.Nearest(2, exclude)
+	if 3 != nearestIndex {
+		t.Fatalf("nearest index lookup incorrect")
+	}
+	if !almostEqual(distance, math.Sqrt(17)) {
+		t.Fatalf("nearest distance incorrect: %v", distance)
+	}
+
+	// Check exclusion
+	exclude[3] = struct{}{}
+	nearestIndex, distance = ld.Nearest(2, exclude)
+	if 1 != nearestIndex {
+		t.Fatalf("nearest index lookup incorrect")
+	}
+}
+
+func TestRouteCost(t *testing.T) {
+	loads := make([]*Load, 2)
+	l0x := NewLocation(0, 0)
+	l0y := NewLocation(3, 4)
+	loads[0] = NewLoad(1, l0x, l0y)
+
+	l1x := NewLocation(3, 5)
+	l1y := NewLocation(6, 9)
+	loads[1] = NewLoad(2, l1x, l1y)
+
+	ld := NewLoadDistance(loads)
+
+	route := make([][]int, 1)
+	route[0] = make([]int, 2)
+	route[0][0] = 1
+	route[0][1] = 2
+
+	cost := ld.RouteCost(route)
+	expectedCost := 500 + 5 + 1 + 5 + math.Sqrt(36+81)
+	if !almostEqual(expectedCost, cost) {
+		t.Fatalf("cost calculation failed. expected %v, got %v", expectedCost, cost)
 	}
 }
